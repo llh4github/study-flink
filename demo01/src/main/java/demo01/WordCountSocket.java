3c75d929b54f1ece55b7e7d3dc0e3379b917c077
llh4github <lilinhong_coding@foxmail.com>
2025-02-10 21:31:47 +0800

    fix: 改从socket流中读取数据

--- demo01/src/main/java/demo01/WordCountSocket.java
+++ demo01/src/main/java/demo01/WordCountSocket.java
@@ -1,30 +1,22 @@
 package demo01;
 
 import llh.studyflink.WordCount;
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.connector.file.src.FileSource;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
-import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 
-public class WordCountExample {
+/**
+ * 从socket端口读取数据进行计词。
+ * nc -lvp 9999
+ */
+public class WordCountSocket {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
-        var filePath = "./word-count.txt";
-        if(args.length > 0){
-            filePath = args[0];
-        }
-        Path path = new Path(filePath);
-        FileSource<String> fileSource = FileSource
-                .forRecordStreamFormat(new TextLineInputFormat(), path)
-                .build();
-        var textStream = environment.fromSource(fileSource, WatermarkStrategy.noWatermarks(), "file-source");
-
+        DataStreamSource<String> textStream = environment.socketTextStream("0.0.0.0", 9999);
         var words = textStream.flatMap((FlatMapFunction<String, String>) (sentence, collector) -> {
             String[] split = sentence.split("\\s+");
             for (String word : split) {
